f182a336eafede654bd56467c636e6c9a5d01249
shotastage <shota.shimazu@magicalsoft.app>
2023-08-07 20:46:08 +0900

    revise

--- R/JPDays.R
+++ R/JPDays.R
@@ -1,3 +1,11 @@
 is_Weekday <- function() {
   print("Hello, world!")
+  if (!is_workspace_exists("jp_governmental_holiday_db.rda")) {
+    jp_governmental_holiday_db <- read.csv("http://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv",
+                                           header = TRUE,
+                                           fileEncoding = "CP932",
+                                           stringsAsFactors = FALSE)
+    save(jp_governmental_holiday_db, file = in_script_workspace("jp_governmental_holiday_db.rda"))
+  }
+
 }

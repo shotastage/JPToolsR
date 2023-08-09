661f9387f5d2392c3195ef3d81513632aa793f88
shotastage <shota.shimazu@magicalsoft.app>
2023-08-09 10:29:40 +0900

    add date converter

--- R/JPDays.R
+++ R/JPDays.R
@@ -1,3 +1,29 @@
+convert_to_posixt <- function(datetime_text) {
+  # Pattern 1: XXXX年X月X日X時X分
+  if (grepl("\\d{4}年\\d{1,2}月\\d{1,2}日\\d{1,2}時\\d{1,2}分", datetime_text)) {
+    datetime <- as.POSIXct(datetime_text, format = "%Y年%m月%d日%H時%M分")
+  }
+  # Pattern 2: XXXX/XX/XX/XX:XX
+  else if (grepl("\\d{4}/\\d{1,2}/\\d{1,2}/\\d{1,2}:\\d{1,2}", datetime_text)) {
+    datetime <- as.POSIXct(datetime_text, format = "%Y/%m/%d/%H:%M")
+  }
+  # Pattern 3: Only date (ex: XXXX年X月X日 or XXXX/XX/XX)
+  else if (grepl("\\d{4}年\\d{1,2}月\\d{1,2}日", datetime_text)) {
+    # Add noon
+    datetime <- as.POSIXct(paste0(datetime_text, "12時00分"), format = "%Y年%m月%d日%H時%M分")
+  }
+  else if (grepl("\\d{4}/\\d{1,2}/\\d{1,2}", datetime_text)) {
+    # Add noon
+    datetime <- as.POSIXct(paste0(datetime_text, "/12:00"), format = "%Y/%m/%d/%H:%M")
+  }
+  else {
+    stop("Unknown datetime pattern.")
+  }
+
+  return(datetime)
+}
+
+
 is_Weekday <- function(timestamp) {
   wd <- tempdir()
   wf <- tempfile()
